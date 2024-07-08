1bdaddfdd9f94128ac8c7f33a09f9c6f0187cdac
kuma <kuma@sonicgarden.jp>
2024-07-08 13:04:31 +0900

    fix: ナビゲーションのテスト修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -147,7 +147,7 @@ RSpec.describe 'Users', type: :system do
           expect(page).to have_link 'レビューランキング一覧', href: '#'
           expect(page).to have_link 'ログイン', href: new_user_session_path
           expect(page).to have_link '新規登録', href: new_user_registration_path
-          expect(page).to have_link 'イベント作成', href: '#'
+          expect(page).to have_link 'イベント作成', href: new_my_event_path
         end
       end
     end
@@ -162,10 +162,10 @@ RSpec.describe 'Users', type: :system do
           expect(page).to have_link 'TechApp', href: root_path
           expect(page).to have_link 'イベント一覧', href: '#'
           expect(page).to have_link 'レビューランキング一覧', href: '#'
-          expect(page).to have_link 'マイページ', href: '#'
+          expect(page).to have_link 'マイページ', href: my_events_path
           expect(page).to have_link '基本情報編集', href: edit_user_registration_path
           expect(page).to have_link 'ログアウト', href: destroy_user_session_path
-          expect(page).to have_link 'イベント作成', href: '#'
+          expect(page).to have_link 'イベント作成', href: new_my_event_path
         end
       end
     end
