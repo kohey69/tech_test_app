42b8e223f937f0f774b9c0769917f9caadfe3a1a
kuma <kuma@sonicgarden.jp>
2024-07-08 13:56:58 +0900

    feat: イベント詳細テスト作成

--- spec/system/my/my_events_spec.rb
+++ spec/system/my/my_events_spec.rb
@@ -16,13 +16,14 @@ RSpec.describe 'My::Events', type: :system do
 
     it '公開・非公開のイベント一覧が表示できること' do
       login_as user, scope: :user
-      _published_event = create(:event, user:, title: '公開イベント')
-      _unpublished_event = create(:event, :unpublished, user:, title: '非公開イベント')
+      published_event = create(:event, user:, title: '公開イベント')
+      unpublished_event = create(:event, :unpublished, user:, title: '非公開イベント')
       visit my_events_path
 
       expect(page).to have_content '公開イベント'
-      expect(page).to have_link '詳細', href: '#'
+      expect(page).to have_link '詳細', href: my_event_path(published_event)
       expect(page).to have_content '非公開イベント'
+      expect(page).to have_link '詳細', href: my_event_path(unpublished_event)
     end
 
     it 'ゲストユーザーがページ遷移できないこと' do
@@ -30,6 +31,33 @@ RSpec.describe 'My::Events', type: :system do
 
       expect(page).to have_content 'ログインもしくはアカウント登録してください。'
       expect(page).to have_current_path new_user_session_path
+    end
+  end
+
+  describe 'マイページ： イベント詳細' do
+    it '公開バッジが表示されること' do
+      event = create(:event, user:)
+      login_as user, scope: :user
+      visit my_event_path(event)
+
+      expect(page).to have_content '公開'
+    end
+
+    it '非公開バッジが表示されること' do
+      event = create(:event, :unpublished, user:)
+      login_as user, scope: :user
+      visit my_event_path(event)
+
+      expect(page).to have_content '非公開'
+    end
+
+    it '編集・削除リンクが表示されていること' do
+      event = create(:event, user:)
+      login_as user, scope: :user
+      visit my_event_path(event)
+
+      expect(page).to have_link '編集する', href: '#'
+      expect(page).to have_link '削除する', href: '#'
     end
   end
 end
