c3ace77a9fdec0979bf70f2d641580f14b7de373
kuma <kuma@sonicgarden.jp>
2024-07-09 17:53:58 +0900

    feat: 主催するイベントにお気に入りできないように

--- app/controllers/events/favorites_controller.rb
+++ app/controllers/events/favorites_controller.rb
@@ -1,4 +1,6 @@
 class Events::FavoritesController < Events::ApplicationController
+  before_action :redirect_if_event_created_by_current_user, only: [:create]
+
   def create
     current_user.favorites.find_or_create_by!(event: @event) # いいね済みの場合は例外を吐かないように
     redirect_to event_path(@event), notice: t('controllers.created')
@@ -8,5 +10,13 @@ class Events::FavoritesController < Events::ApplicationController
     favorite = current_user.favorites.find_by(event: @event)
     favorite&.destroy # windowを複数開いていいねを解除した時に例外を吐かないように
     redirect_to event_path(@event), notice: t('controllers.destroyed')
+  end
+
+  private
+
+  def redirect_if_event_created_by_current_user
+    if @event.user == current_user
+      redirect_to event_path(@event), alert: 'イベント主催者はお気に入り登録できません'
+    end
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,11 @@ class Favorite < ApplicationRecord
   belongs_to :event
 
   validates :user_id, uniqueness: { scope: :event }
+  validate :must_not_be_created_by_user
+
+  def must_not_be_created_by_user
+    if self.event.user == self.user
+      errors.add(:base, '主催者はお気に入りできません')
+    end
+  end
 end
