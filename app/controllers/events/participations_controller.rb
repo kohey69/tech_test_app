dc52b023c22072b081921b121b8f718cea3de0cd
kuma <kuma@sonicgarden.jp>
2024-07-09 14:47:47 +0900

    fix: コメント修正

--- app/controllers/events/participations_controller.rb
+++ app/controllers/events/participations_controller.rb
@@ -2,12 +2,12 @@ class Events::ParticipationsController < Events::ApplicationController
   before_action :redirect_if_event_created_by_current_user, only: [:create]
 
   def create
-    current_user.participations.find_or_create_by!(event: @event) # お気に入り済みの場合は例外を吐かないように
+    current_user.participations.find_or_create_by!(event: @event) # 参加済みの場合は例外を吐かないように
     redirect_to event_path(@event), notice: t('controllers.created')
   end
 
   def destroy
-    participation = current_user.participations.find_by(event: @event) # windowを複数開いてお気に入りを解除した時に例外を吐かないように
+    participation = current_user.participations.find_by(event: @event) # windowを複数開いてキャンセルした時に例外を吐かないように
     participation&.destroy!
     redirect_to event_path(@event), notice: t('controllers.destroyed')
   end
