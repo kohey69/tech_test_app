ab005e85df2d6b03860b5410db97d659239a7a45
kuma <kuma@sonicgarden.jp>
2024-07-10 12:10:18 +0900

    fix: 開催済みのイベントは参加登録できないように

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -56,15 +56,13 @@ RSpec.describe Event, type: :model do
   describe '#not_started' do
     context '現在日時が7月1日0時の場合' do
       before do
-        event = create(:event, :with_user, title: '取得できるイベント')
-        other_event = create(:event, :with_user, title: '取得できないイベント')
-        event.update_columns(start_at: Time.zone.local(2024, 7, 1, 0, 1))
-        other_event.update_columns(start_at: Time.zone.local(2024, 6, 30, 23, 59))
         travel_to Time.zone.local(2024, 7, 1, 0, 0)
+        create(:event, :skip_validate, :with_user, title: '開始前のイベント', start_at: Time.zone.local(2024, 7, 1, 0, 1))
+        create(:event, :skip_validate, :with_user, title: '開始済みのイベント', start_at: Time.zone.local(2024, 7, 1, 0, 0))
       end
 
-      it '7月1日0時までに開催されたイベントは取得されないこと' do
-        expect(Event.not_started.pluck(:title)).to contain_exactly('取得できるイベント')
+      it '現在時刻以降に開始したイベントは取得されないこと' do
+        expect(Event.not_started.pluck(:title)).to contain_exactly('開始前のイベント')
       end
     end
   end
@@ -127,6 +125,26 @@ RSpec.describe Event, type: :model do
 
       it '開始日が早い順に並べられること' do
         expect(Event.order_by_participations_count.pluck(:title)).to contain_exactly('開始日時が7月1日で参加者が3人のイベント', '開始日時が7月2日で参加者が3人のイベント', '開始日時が7月3日で参加者が3人のイベント')
+      end
+    end
+  end
+
+  describe '#not_started?' do
+    before { travel_to Time.zone.local(2024, 6, 13, 0, 0) }
+
+    context 'イベント開始前の場合' do
+      let(:event) { create(:event, :with_user, start_at: Time.zone.local(2024, 6, 13, 0, 1)) }
+
+      it 'falseを返すこと' do
+        expect(event.not_started?).to be true
+      end
+    end
+
+    context 'イベント開始済みの場合' do
+      let(:event) { create(:event, :with_user, start_at: Time.zone.local(2024, 6, 13, 0, 0)) }
+
+      it 'falseを返すこと' do
+        expect(event.not_started?).to be false
       end
     end
   end
