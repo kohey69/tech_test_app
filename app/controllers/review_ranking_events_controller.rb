a8d4047e3626b76ba0f806f89b9d97a4b3583951
kuma <kuma@sonicgarden.jp>
2024-07-11 16:10:54 +0900

    feat: レビューランキングをゲストユーザーも参照できる様に

--- app/controllers/review_ranking_events_controller.rb
+++ app/controllers/review_ranking_events_controller.rb
@@ -1,4 +1,6 @@
 class ReviewRankingEventsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     @events = Event.with_category(params[:category]).order_by_review_score.page(params[:page]).per(10)
   end
