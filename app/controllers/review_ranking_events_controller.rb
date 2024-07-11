e0bb82a7c28b268a93e2d9f97a856c28de76782d
kuma <kuma@sonicgarden.jp>
2024-07-11 22:01:30 +0900

    fix: 25件を1ページに表示する

--- app/controllers/review_ranking_events_controller.rb
+++ app/controllers/review_ranking_events_controller.rb
@@ -2,6 +2,6 @@ class ReviewRankingEventsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @events = Event.published.ended.with_category(params[:category]).order_by_review_score.page(params[:page]).per(10)
+    @events = Event.published.ended.with_category(params[:category]).order_by_review_score.page(params[:page])
   end
 end
